32dc8118562dc1b745e1b344d9d105cfe969b0af
Pantelispanka <pantelispanka@gmail.com>
2021-10-11 12:16:20 +0300

    configs

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,8 +1,10 @@
 export let Config = {
-   "JaqpotBase":"https://api.jaqpot.org/jaqpot/services",
+    "JaqpotBase":"https://squonkpotapi.jaqpot.org/jaqpot/services",    
+//    "JaqpotBase":"https://api.jaqpot.org/jaqpot/services",
 //    "JaqpotBase":"https://devapi.jaqpot.org/jaqpot/services",
     // 'JaqpotBase': 'http://localhost:8080/jaqpot/services',
     // 'JaqpotBase': 'https://api-jaqpot.prod.openrisknet.org/jaqpot/services',
-    "AccountsApi":"https://accountsapi.jaqpot.org",
+    // "AccountsApi":"https://accountsapi.jaqpot.org",
+    "AccountsApi":"https://scountsapi.jaqpot.org",
     'notif_poll': true
 };
