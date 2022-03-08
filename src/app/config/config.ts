ff993a2f1291f4b2e564b24c4ff7836acfad12a8
Pantelispanka <pantelispanka@gmail.com>
2022-03-08 15:31:26 +0200

    scale features

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,8 +1,18 @@
 export let Config = {
+<<<<<<< Updated upstream
     // "JaqpotBase":"https://squonkpotapi.jaqpot.org/jaqpot/services",    
    "JaqpotBase":"https://api.jaqpot.org/jaqpot/services",
 //    "JaqpotBase":"https://devapi.jaqpot.org/jaqpot/services",
+=======
+    //"JaqpotBase":"https://squonkpotapi.jaqpot.org/jaqpot/services",    
+    //"JaqpotBase":"https://api.jaqpot.org/jaqpot/services",
+    //"JaqpotBase":"https://devapi.jaqpot.org/jaqpot/services",
+>>>>>>> Stashed changes
     // 'JaqpotBase': 'http://localhost:8080/jaqpot/services',
+    //"JaqpotBase":"https://squonkpotapi.jaqpot.org/jaqpot/services",    
+    "JaqpotBase":"https://api.jaqpot.org/jaqpot/services",
+    //'JaqpotBase': 'http://localhost:8080/jaqpot/services',
+    //"JaqpotBase":"https://devapi.jaqpot.org/jaqpot/services",
     // 'JaqpotBase': 'https://api-jaqpot.prod.openrisknet.org/jaqpot/services',
     "AccountsApi":"https://accountsapi.jaqpot.org",
     // "AccountsApi":"https://scountsapi.jaqpot.org",
--- src/app/jaqpot-client/factories/dataset-factory.service.ts
+++ src/app/jaqpot-client/factories/dataset-factory.service.ts
@@ -30,6 +30,10 @@ export class DatasetFactoryService {
         rows.splice(remove, 1)
       }
     })
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     // let ids = rows[0].split(/,|;/);
     let ids = []
     this.csvToArray(rows[0]).forEach(id => {
@@ -275,7 +279,15 @@ export class DatasetFactoryService {
     this.csvToArray(rows[0]).forEach(id => {
       ids.push(id)
     })
+<<<<<<< Updated upstream
 
+=======
+    // rows[0].split(/,|;/).forEach(id => {
+    //   console.log(id)
+    //   let cleanedid = id.replace(/^"|"$/g, '')
+    //   ids.push(cleanedid)
+    // });
+>>>>>>> Stashed changes
     rows.splice(0,1)
     let i = 0
     rows.forEach(row => {
@@ -353,6 +365,13 @@ export class DatasetFactoryService {
     this.csvToArray(rows[0]).forEach(id => {
       ids.push(id)
     })
+<<<<<<< Updated upstream
+=======
+    // rows[0].split(/,|;/).forEach(id => {
+    //   let cleanedid = id.replace(/^"|"$/g, '')
+    //   ids.push(cleanedid)
+    // });
+>>>>>>> Stashed changes
     rows.splice(0,1)
     let i = 0
     rows.forEach(row => {
